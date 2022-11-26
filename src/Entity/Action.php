ba809fa97a8fc557ad30552dc620e1d951a88eae
Andrew Lehman <aplehm@gmail.com>
2022-11-26 16:44:34 -0500

    Add Action entity and repo

---
+++ src/Entity/Action.php
@@ -0,0 +1,97 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\ActionRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: ActionRepository::class)]
+#[ORM\Table(name: '`action`')]
+class Action
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\ManyToOne]
+    #[ORM\JoinColumn(nullable: false)]
+    private ?History $history = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $operation = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $entityType = null;
+
+    #[ORM\Column]
+    private ?int $entityId = null;
+
+    #[ORM\Column(length: 1000)]
+    private ?string $description = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getHistory(): ?History
+    {
+        return $this->history;
+    }
+
+    public function setHistory(?History $history): self
+    {
+        $this->history = $history;
+
+        return $this;
+    }
+
+    public function getOperation(): ?string
+    {
+        return $this->operation;
+    }
+
+    public function setOperation(string $operation): self
+    {
+        $this->operation = $operation;
+
+        return $this;
+    }
+
+    public function getEntityType(): ?string
+    {
+        return $this->entityType;
+    }
+
+    public function setEntityType(string $entityType): self
+    {
+        $this->entityType = $entityType;
+
+        return $this;
+    }
+
+    public function getEntityId(): ?int
+    {
+        return $this->entityId;
+    }
+
+    public function setEntityId(int $entityId): self
+    {
+        $this->entityId = $entityId;
+
+        return $this;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(string $description): self
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+}
