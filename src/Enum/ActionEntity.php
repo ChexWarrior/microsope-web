ef3902e3a83745fab3bd0e1e5d92a928f554186b
Andrew Lehman <aplehm@gmail.com>
2022-11-27 14:51:24 -0500

    Rename enums

--- src/Enum/ActionEntity.php
+++ src/Enum/ActionEntity.php
@@ -1,6 +1,6 @@
 <?php declare(strict_types=1);
 
-enum EntityType: string
+enum ActionEntity: string
 {
     case History = 'history';
     case Event = 'event';
--- src/Enum/ActionOperation.php
+++ src/Enum/ActionOperation.php
@@ -1,6 +1,6 @@
 <?php declare(strict_types=1);
 
-enum EntityOperation: string
+enum ActionOperation: string
 {
     case CREATE = 'create';
     case EDIT = 'edit';
