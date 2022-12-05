a7f1ac5d6d968fc48a7d5ccbde78a9d95e934e4c
Andrew Lehman <aplehm@gmail.com>
2022-12-04 20:00:59 -0500

    Create dummy players and history

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,6 +2,8 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\History;
+use App\Entity\Player;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 
@@ -9,9 +11,20 @@ class AppFixtures extends Fixture
 {
     public function load(ObjectManager $manager): void
     {
-        // $product = new Product();
-        // $manager->persist($product);
+        $faker = \Faker\Factory::create();
+        $players = [];
 
+        for ($i = 0; $i < 3; $i += 1) {
+            $player = new Player(name: $faker->firstName(), history: null, active: true, legacy: null, isLens: false);
+            $players[] = $player;
+            $manager->persist($player);
+        }
+
+        $history = new History();
+        $history->setDescription($faker->text());
+        array_walk($players, fn (Player $p) => $history->addPlayer($p));
+
+        $manager->persist($history);
         $manager->flush();
     }
 }
