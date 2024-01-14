0cf61d576f56746574a2b96e30e2086e0391169e
Andrew Lehman <aplehm@gmail.com>
2024-01-14 09:40:59 -0500

    Add main group id to first fixture

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -9,10 +9,16 @@ use App\Entity\Player;
 use App\Entity\Scene;
 use App\Enum\Tone;
 use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Bundle\FixturesBundle\FixtureGroupInterface;
 use Doctrine\Persistence\ObjectManager;
 
-class AppFixtures extends Fixture
+class AppFixtures extends Fixture implements FixtureGroupInterface
 {
+    public static function getGroups(): array
+    {
+        return ['main'];
+    }
+
     public function load(ObjectManager $manager): void
     {
         $numPlayers = 5;
