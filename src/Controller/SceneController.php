375ad74628a33a4136e202ffd4de7e090eef0aa9
Andrew Lehman <aplehm@gmail.com>
2024-01-14 17:42:51 -0500

    Add htmx event for showing scene events

--- src/Controller/SceneController.php
+++ src/Controller/SceneController.php
@@ -5,9 +5,11 @@ namespace App\Controller;
 use App\Entity\Event;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
 
 class SceneController extends AbstractController
 {
+    #[Route('/scenes/{id}', name: 'scenes', methods: 'GET')]
     public function scenes(Event $event): Response
     {
         return $this->render('partials/scene.html.twig', [
