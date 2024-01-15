3a65ed24878a8801341e1564cd9f7022732d0f8c
Andrew Lehman <aplehm@gmail.com>
2024-01-15 13:27:15 -0500

    Add base entity class Term

---
+++ src/Entity/Term.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Entity;
+
+use App\Enum\Tone;
+use Doctrine\ORM\Mapping as ORM;
+use Doctrine\ORM\Mapping\MappedSuperclass;
+
+#[MappedSuperclass()]
+abstract class Term
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column]
+    private ?int $place = null;
+
+    #[ORM\Column(length: 255, enumType: Tone::class)]
+    private ?Tone $tone = null;
+
+    #[ORM\Column(length: 1000)]
+    private ?string $description = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getPlace(): ?int
+    {
+        return $this->place;
+    }
+
+    public function setPlace(int $place): self
+    {
+        $this->place = $place;
+
+        return $this;
+    }
+
+    public function getTone(): Tone
+    {
+        return $this->tone;
+    }
+
+    public function setTone(Tone $tone): self
+    {
+        $this->tone = $tone;
+
+        return $this;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(string $description): self
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+}
