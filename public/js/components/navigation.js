c73c562ee050129961d1401e863a3b3f6db8f84f
Andrew Lehman <aplehm@gmail.com>
2024-01-16 18:38:01 -0500

    Add js for focusing on selected period/event

---
+++ public/js/components/navigation.js
@@ -0,0 +1,36 @@
+htmx.onLoad(function(content) {
+  // Grab selectors for period and event
+  const periodSelect = document.querySelector('select.periods');
+  const eventSelect = document.querySelector('select.events');
+
+  if (periodSelect) {
+    periodSelect.addEventListener('change', function (e) {
+      // Set focus to the period.
+      focusCard(`period-${e.target.value}`);
+    });
+  }
+
+  if (eventSelect) {
+    eventSelect.addEventListener('change', function (e) {
+      // Set focus to the event.
+      focusCard(`event-${e.target.value}`);
+
+    });
+  }
+});
+
+function focusCard(id) {
+  // Scroll to card.
+  const focusedCard = document.querySelector(`#${id} > .card`);
+  focusedCard.scrollIntoView();
+
+  // Remove highlight from any previous card.
+  const highlightedCards = document.querySelectorAll('.card');
+  highlightedCards.forEach(function(card) {
+    card.classList.remove('focus-border');
+  });
+
+  // Add highlight to current card.
+  focusedCard.classList.add('focus-border');
+
+}
