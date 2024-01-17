649744a21ae8039ae2e91fdd54c0ce2ad73d9df1
Andrew Lehman <aplehm@gmail.com>
2024-01-16 20:32:30 -0500

    Add default option for scrolling to start

--- public/js/components/navigation.js
+++ public/js/components/navigation.js
@@ -1,28 +1,34 @@
 htmx.onLoad(function(content) {
   // Grab selectors for period and event
   const cardSelector = document.querySelector('#card-focus');
+  const boardSelector = document.querySelector('#board');
 
   if (cardSelector) {
     cardSelector.addEventListener('change', function(e) {
-      focusCard(e.target.value);
+      focusCard(e.target.value, boardSelector);
     });
   }
 });
 
-function focusCard(id) {
-  // Scroll to card.
-  const focusedCard = document.querySelector(`#${id} > .card`);
-  focusedCard.scrollIntoView({
-    'inline': 'center',
-    'block': 'center',
-  });
-
+function focusCard(id, board) {
   // Remove highlight from any previous card.
   const highlightedCards = document.querySelectorAll('.card');
   highlightedCards.forEach(function(card) {
     card.classList.remove('focus-border');
   });
 
-  // Add highlight to current card.
-  focusedCard.classList.add('focus-border');
+  if (id) {
+    // Scroll to card.
+    const focusedCard = document.querySelector(`#${id} > .card`);
+    focusedCard.scrollIntoView({
+      'inline': 'center',
+      'block': 'center',
+    });
+
+    // Add highlight to current card.
+    focusedCard.classList.add('focus-border');
+  }
+  else {
+    board.scrollTo(0, 0);
+  }
 }
